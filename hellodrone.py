6690ac177a83fae195e37b86ccc9ea07ba4c7fe5
nicdiazf <49282203+nicdiazf@users.noreply.github.com>
2021-05-14 19:04:29 -0600

    Add files via upload

---
+++ hellodrone.py
@@ -0,0 +1,28 @@
+import dronekit_sitl
+
+print "Simulator start"
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
+
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+
+# Connect to the Vehicle.
+print("Connecting to vehicle on: %s" % (connection_string,))
+vehicle = connect(connection_string, wait_ready=True)
+
+# Vehicle Attributes (state)
+print "Vehicle attributes:"
+print " GPS: %s" % vehicle.gps_0
+print " Batt: %s" % vehicle.battery
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Mode: %s" % vehicle.mode.name    # settable
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+# Stop simulator
+sitl.stop()
+print("Completed")
